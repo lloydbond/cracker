44f697577694be9646224e8b41fc1a785e9c088c
Lloyd Bond <lloyd.bond@gmail.com>
2024-10-14 07:43:15 -0400

    initial parsing of Makefile rules, buttons, and editor starter code cleanup done

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 use iced::highlighter;
-use iced::keyboard;
-use iced::widget::{
-    self, button, column, container, horizontal_space, pick_list, row, text, text_editor, toggler,
-    tooltip,
-};
-use iced::{Center, Element, Fill, Font, Task, Theme};
+use iced::widget::Column;
+use iced::widget::{self, button, column, container, row, text, tooltip};
+use iced::{Center, Element, Font, Task, Theme};
 
-use std::ffi;
+// use peg;
+use makefile_lossless::Makefile;
+use std::fs;
+
 use std::io;
-use std::path::{Path, PathBuf};
 use std::process::Command;
-use std::sync::Arc;
 
 pub fn main() -> iced::Result {
     iced::application("Editor - Iced", Editor::update, Editor::view)
@@ -21,64 +19,32 @@ pub fn main() -> iced::Result {
 }
 
 struct Editor {
-    file: Option<PathBuf>,
-    content: text_editor::Content,
     theme: highlighter::Theme,
-    word_wrap: bool,
-    is_loading: bool,
-    is_dirty: bool,
+    targets: Vec<String>,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
-    ActionPerformed(text_editor::Action),
     ThemeSelected(highlighter::Theme),
-    WordWrapToggled(bool),
-    NewFile,
-    OpenFile,
-    FileOpened(Result<(PathBuf, Arc<String>), Error>),
-    SaveFile,
-    FileSaved(Result<PathBuf, Error>),
     TaskMake(String),
+    LoadMakeTargets,
 }
 
 impl Editor {
     fn new() -> (Self, Task<Message>) {
         (
             Self {
-                file: None,
-                content: text_editor::Content::new(),
                 theme: highlighter::Theme::SolarizedDark,
-                word_wrap: true,
-                is_loading: true,
-                is_dirty: false,
+                targets: Vec::new(),
             },
-            Task::batch([
-                Task::perform(
-                    load_file(format!("{}/Makefile", env!("PWD"))),
-                    Message::FileOpened,
-                ),
-                widget::focus_next(),
-            ]),
+            Task::batch([Task::done(Message::LoadMakeTargets), widget::focus_next()]),
         )
     }
 
     fn update(&mut self, message: Message) -> Task<Message> {
         match message {
-            Message::ActionPerformed(action) => {
-                self.is_dirty = self.is_dirty || action.is_edit();
-
-                self.content.perform(action);
-
-                Task::none()
-            }
             Message::ThemeSelected(theme) => {
                 self.theme = theme;
-
-                Task::none()
-            }
-            Message::WordWrapToggled(word_wrap) => {
-                self.word_wrap = word_wrap;
 
                 Task::none()
             }
@@ -90,53 +56,27 @@ impl Editor {
 
                 Task::none()
             }
-            Message::NewFile => {
-                if !self.is_loading {
-                    self.file = None;
-                    self.content = text_editor::Content::new();
+            Message::LoadMakeTargets => {
+                let f = fs::File::open("Makefile").unwrap();
+                let result = Makefile::read_relaxed(f);
+                if result.is_ok() {
+                    let makefile: makefile_lossless::Makefile = result.unwrap();
+                    println!("{}", makefile.rules().count());
+
+                    self.targets.clear();
+                    for rule in makefile.rules() {
+                        for target in rule.targets() {
+                            self.targets.push(target.clone());
+                        }
+                    }
                 }
 
-                Task::none()
-            }
-            Message::OpenFile => {
-                if self.is_loading {
-                    Task::none()
-                } else {
-                    self.is_loading = true;
-
-                    Task::perform(open_file(), Message::FileOpened)
-                }
-            }
-            Message::FileOpened(result) => {
-                self.is_loading = false;
-                self.is_dirty = false;
-
-                if let Ok((path, contents)) = result {
-                    self.file = Some(path);
-                    self.content = text_editor::Content::with_text(&contents);
-                }
-
-                Task::none()
-            }
-            Message::SaveFile => {
-                if self.is_loading {
-                    Task::none()
-                } else {
-                    self.is_loading = true;
-
-                    Task::perform(
-                        save_file(self.file.clone(), self.content.text()),
-                        Message::FileSaved,
-                    )
-                }
-            }
-            Message::FileSaved(result) => {
-                self.is_loading = false;
-
-                if let Ok(path) = result {
-                    self.file = Some(path);
-                    self.is_dirty = false;
-                }
+                // peg::parser!{
+                //     grammar list_parser() for str {
+                //         rule number() -> u32
+                //         = "[" l:(number() ** ",") "]" {l}
+                //     }
+                // }
 
                 Task::none()
             }
@@ -144,81 +84,20 @@ impl Editor {
     }
 
     fn view(&self) -> Element<Message> {
-        let controls = row![
-            action(new_icon(), "New file", Some(Message::NewFile)),
-            action(
-                open_icon(),
-                "Open file",
-                (!self.is_loading).then_some(Message::OpenFile)
-            ),
-            action(
-                save_icon(),
-                "Save file",
-                self.is_dirty.then_some(Message::SaveFile)
-            ),
-            horizontal_space(),
-            toggler(self.word_wrap)
-                .label("Word Wrap")
-                .on_toggle(Message::WordWrapToggled),
-            pick_list(
-                highlighter::Theme::ALL,
-                Some(self.theme),
-                Message::ThemeSelected
-            )
-            .text_size(14)
-            .padding([5, 10])
-        ]
-        .spacing(10)
-        .align_y(Center);
+        let controls = row![].spacing(10).align_y(Center);
 
-        let status = row![
-            text(if let Some(path) = &self.file {
-                let path = path.display().to_string();
+        let status = row![].spacing(10);
 
-                if path.len() > 60 {
-                    format!("...{}", &path[path.len() - 40..])
-                } else {
-                    path
-                }
-            } else {
-                String::from("New file")
-            }),
-            horizontal_space(),
-            text({
-                let (line, column) = self.content.cursor_position();
+        let mut targets = Vec::new();
+        for target in self.targets.iter() {
+            targets.push(action(
+                start_icon(),
+                target,
+                Some(Message::TaskMake(target.to_string())),
+            ));
+        }
 
-                format!("{}:{}", line + 1, column + 1)
-            })
-        ]
-        .spacing(10);
-        let te = text_editor(&self.content)
-            .height(Fill)
-            .on_action(Message::ActionPerformed)
-            .wrapping(if self.word_wrap {
-                text::Wrapping::Word
-            } else {
-                text::Wrapping::None
-            })
-            .highlight(
-                self.file
-                    .as_deref()
-                    .and_then(Path::extension)
-                    .and_then(ffi::OsStr::to_str)
-                    .unwrap_or("rs"),
-                self.theme,
-            )
-            .key_binding(|key_press| match key_press.key.as_ref() {
-                keyboard::Key::Character("s") if key_press.modifiers.command() => {
-                    Some(text_editor::Binding::Custom(Message::SaveFile))
-                }
-                _ => text_editor::Binding::from_key_press(key_press),
-            });
-        let target = action(
-            new_icon(),
-            "target",
-            Some(Message::TaskMake(String::from("hello"))),
-        );
-        column![controls, te, target, status,]
+        column![controls, Column::from_vec(targets), status,]
             .spacing(10)
             .padding(10)
             .into()
@@ -237,47 +116,6 @@ impl Editor {
 pub enum Error {
     DialogClosed,
     IoError(io::ErrorKind),
-}
-
-async fn open_file() -> Result<(PathBuf, Arc<String>), Error> {
-    let picked_file = rfd::AsyncFileDialog::new()
-        .set_title("Open a text file...")
-        .pick_file()
-        .await
-        .ok_or(Error::DialogClosed)?;
-
-    load_file(picked_file).await
-}
-
-async fn load_file(path: impl Into<PathBuf>) -> Result<(PathBuf, Arc<String>), Error> {
-    let path = path.into();
-
-    let contents = tokio::fs::read_to_string(&path)
-        .await
-        .map(Arc::new)
-        .map_err(|error| Error::IoError(error.kind()))?;
-
-    Ok((path, contents))
-}
-
-async fn save_file(path: Option<PathBuf>, contents: String) -> Result<PathBuf, Error> {
-    let path = if let Some(path) = path {
-        path
-    } else {
-        rfd::AsyncFileDialog::new()
-            .save_file()
-            .await
-            .as_ref()
-            .map(rfd::FileHandle::path)
-            .map(Path::to_owned)
-            .ok_or(Error::DialogClosed)?
-    };
-
-    tokio::fs::write(&path, contents)
-        .await
-        .map_err(|error| Error::IoError(error.kind()))?;
-
-    Ok(path)
 }
 
 fn action<'a, Message: Clone + 'a>(
@@ -300,16 +138,8 @@ fn action<'a, Message: Clone + 'a>(
     }
 }
 
-fn new_icon<'a, Message>() -> Element<'a, Message> {
-    icon('\u{0e800}')
-}
-
-fn save_icon<'a, Message>() -> Element<'a, Message> {
-    icon('\u{0e801}')
-}
-
-fn open_icon<'a, Message>() -> Element<'a, Message> {
-    icon('\u{0f115}')
+fn start_icon<'a, Message>() -> Element<'a, Message> {
+    icon('\u{0e802}')
 }
 
 fn icon<'a, Message>(codepoint: char) -> Element<'a, Message> {
