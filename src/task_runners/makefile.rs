335c903242ebbf9f7f560bc46997a2b7730de6f6
Lloyd Bond <lloyd.bond@gmail.com>
2024-11-16 05:43:33 -0500

    reorganize makefile modules

--- src/task_runners/makefile.rs
+++ src/task_runners/makefile.rs
@@ -23,7 +23,7 @@ mod tests {
     #[test]
     fn test_one_target_rule() {
         let rule = "target:";
-        let actual: Vec<String> = makefile::Targets(rule).ok().unwrap();
+        let actual: Vec<String> = grammar::Targets(rule).ok().unwrap();
         let expected: Vec<String> = vec![String::from("target")];
         assert_eq!(actual, expected);
     }
@@ -31,7 +31,7 @@ mod tests {
     #[test]
     fn test_multi_target_rule() {
         let rule = "target t target_three:";
-        let actual: Vec<String> = makefile::Targets(rule).ok().unwrap();
+        let actual: Vec<String> = grammar::Targets(rule).ok().unwrap();
         let expected: Vec<String> = vec![
             String::from("target"),
             String::from("t"),
@@ -43,7 +43,7 @@ mod tests {
     #[test]
     fn test_multi_target_variable_spacing_rule() {
         let rule = " target  t       target_three:";
-        let actual: Vec<String> = makefile::Targets(rule).ok().unwrap();
+        let actual: Vec<String> = grammar::Targets(rule).ok().unwrap();
         let expected: Vec<String> = vec![
             String::from("target"),
             String::from("t"),
@@ -55,7 +55,7 @@ mod tests {
     #[test]
     fn test_targets_with_num() {
         let rule = "1target target1:";
-        let actual: Vec<String> = makefile::Targets(rule).ok().unwrap();
+        let actual: Vec<String> = grammar::Targets(rule).ok().unwrap();
         let expected: Vec<String> = vec![String::from("1target"), String::from("target1")];
         assert_eq!(actual, expected);
     }
@@ -63,7 +63,7 @@ mod tests {
     #[test]
     fn test_target_with_dot() {
         let rule = "target.o:";
-        let actual: Vec<String> = makefile::Targets(rule).ok().unwrap();
+        let actual: Vec<String> = grammar::Targets(rule).ok().unwrap();
         let expected: Vec<String> = vec![String::from("target.o")];
         assert_eq!(actual, expected);
     }
@@ -71,7 +71,7 @@ mod tests {
     #[test]
     fn test_target_inline_recipe_rule() {
         let rule = "target: ; tail -f /dev/null";
-        let actual: Vec<String> = makefile::Targets(rule).ok().unwrap();
+        let actual: Vec<String> = grammar::Targets(rule).ok().unwrap();
         let expected: Vec<String> = vec![String::from("target")];
         assert_eq!(actual, expected);
     }
@@ -79,7 +79,7 @@ mod tests {
     #[test]
     fn test_ignore_phony() {
         let rule = ".PHONY: target1 target2";
-        let actual = makefile::Targets(rule).ok();
+        let actual = grammar::Targets(rule).ok();
         let expected = None;
         assert_eq!(actual, expected);
     }
@@ -87,7 +87,7 @@ mod tests {
     #[test]
     fn test_ignore_guard() {
         let rule = "_guard: target1 target2";
-        let actual = makefile::Targets(rule).ok();
+        let actual = grammar::Targets(rule).ok();
         let expected = None;
         assert_eq!(actual, expected);
     }
@@ -95,7 +95,7 @@ mod tests {
     #[test]
     fn test_ignore_variable() {
         let rule = "files := file1 file2";
-        let actual = makefile::Targets(rule).ok();
+        let actual = grammar::Targets(rule).ok();
         let expected = None;
         assert_eq!(actual, expected);
     }
@@ -103,7 +103,7 @@ mod tests {
     #[test]
     fn test_ignore_wildcard_catchall() {
         let rule = "%::";
-        let actual = makefile::Targets(rule).ok();
+        let actual = grammar::Targets(rule).ok();
         let expected = None;
         assert_eq!(actual, expected);
     }
@@ -111,7 +111,7 @@ mod tests {
     #[test]
     fn test_ignore_tabbed_target() {
         let rule = "\ttarget:";
-        let actual = makefile::Targets(rule).ok();
+        let actual = grammar::Targets(rule).ok();
         let expected = None;
         assert_eq!(actual, expected);
     }
@@ -119,7 +119,7 @@ mod tests {
     #[test]
     fn test_recipe_should_err() {
         let rule = "\t@echo \"Hello Cracker\"";
-        assert!(makefile::Targets(rule).is_err());
+        assert!(grammar::Targets(rule).is_err());
     }
 }
 }
